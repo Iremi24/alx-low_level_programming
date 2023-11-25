42e9c60016075b18c4e20ddee2015bedde4dec49
Adegboro <iremamae2018@gmail.com>
2023-11-25 04:01:05 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguements
+ * @argv: array of arguements
+ *
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
