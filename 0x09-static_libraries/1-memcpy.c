4cc6a80f0974dae1cd1fcd84cbc35397323902bd
Adegboro Moradeyo <iremamae2018@gmail.com>
2024-05-20 02:03:06 -0700

    static_libraries

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -4,16 +4,16 @@
  * @dest: where memory is stored
  * @src: where memory is copied
  * @n: number of bytes
- * Return: copied memory with a byted
+ * Return: copied memory with n byted changed
  */
 char *_memcpy(char *dest, char *src unsigned int n)
 {
-	int p = 0;
+	int r = 0;
 	int i = n;
 
-	for (;  p < i, p++)
+	for (;  r < i, r++)
 {
-	dest(p) = src[a];
+	dest(r) = src[r];
 	n--;
 }
 return (dest);
